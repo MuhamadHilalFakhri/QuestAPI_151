8e57d8d480b9077935380e3f20c17bfd9fc89244
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 17:43:54 +0700

    membuat fungsi MahasiswaApp

--- app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
+++ app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
@@ -4,10 +4,11 @@ import android.app.Application
 import com.example.pertemuan12.ui.AppContainer
 import com.example.pertemuan12.ui.MahasiswaContainer
 
-class MahasiswaApplications: Application()
-{ lateinit var container: AppContainer
-override fun onCreate() {
-    super.onCreate()
-    container = MahasiswaContainer()
-}
+class MahasiswaApplications: Application() {
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
 }
