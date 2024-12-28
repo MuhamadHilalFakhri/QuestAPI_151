830af13a81781494e89be80bac162a799e2c363a
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 15:17:23 +0700

    membuat MahasiswaRepository

--- app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
+++ app/src/main/java/com/example/pertemuan12/repository/MahasiswaRepository.kt
@@ -1,2 +1,56 @@
 package com.example.pertemuan12.repository
 
+
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.service.MahasiswaService
+import okio.IOException
+
+interface MahasiswaRepository{
+    suspend fun  getMahasiswa():List<Mahasiswa>
+
+    suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    suspend fun updateMahasiswa(nim: String,mahasiswa: Mahasiswa)
+
+    suspend fun deleteMahasiswa(nim: String)
+
+    suspend fun getMahasiswabyNim(nim: String):Mahasiswa
+}
+
+class NetworkMahasiswaRepository(
+    private val mahasiswaApiService: MahasiswaService
+) : MahasiswaRepository {
+    override suspend fun getMahasiswa(): List<Mahasiswa> =
+        mahasiswaApiService.getAllMahasiswa()
+
+
+    override suspend fun insertMahasiswa(mahasiswa: Mahasiswa) {
+        mahasiswaApiService.insertMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMahasiswa(nim: String, mahasiswa: Mahasiswa) {
+        mahasiswaApiService.updateMahasiswa(nim, mahasiswa)
+    }
+
+    override suspend fun deleteMahasiswa(nim: String) {
+        try {
+            val response = mahasiswaApiService.deleteMahasiswa(nim)
+            if (!response.isSuccessful)
+                throw IOException(
+                    "Failed to delete Mahasiswa. PHP Status Code: " +
+                            "${response.code()}"
+                )
+        }else {
+            response.message()
+            println(response.message)
+        }
+    }catch (e:Exception)
+    {
+        throw e
+    }
+
+    override suspend fun getMahasiswabyNim(nim: String): Mahasiswa {
+        return mahasiswaApiService.getMahasiswabyNim(nim)
+    }
+
+}
