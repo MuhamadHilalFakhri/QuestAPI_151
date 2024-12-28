004bacd70431e6de6a9b47261761bae1d660cb27
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 15:55:11 +0700

    membuat data class InsertUiEvent

--- app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/InsertViewModel.kt
@@ -13,3 +13,12 @@ class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
 }
 
 
+
+data class InsertUiEvent(
+    val nim:String = "",
+    val nama:String = "",
+    val alamat:String = "",
+    val jenisKelamin:String = "",
+    val kelas:String = "",
+    val angkatan:String = "",
+)
