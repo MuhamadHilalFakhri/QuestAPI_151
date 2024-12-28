404098c71604b4d1c1e035165ebd8ac73ce06a57
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 15:41:29 +0700

    membuat class MahasiswaApplications

---
+++ app/src/main/java/com/example/pertemuan12/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.example.pertemuan12
+
+import android.app.Application
+import com.example.pertemuan12.ui.AppContainer
+import com.example.pertemuan12.ui.MahasiswaContainer
+
+class MahasiswaApplications: Application()
+{ lateinit var container: AppContainer
+override fun onCreate() {
+    super.onCreate()
+    container = MahasiswaContainer()
+}
+}
