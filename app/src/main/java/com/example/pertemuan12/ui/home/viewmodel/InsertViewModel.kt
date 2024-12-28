1aefecd74333489a80026d7e3930fe21fe378964
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 15:50:16 +0700

    membuat class InsertViewModel

---
+++ app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/InsertViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.pertemuan12.ui.home.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel(){
+
+
+}
+
+
