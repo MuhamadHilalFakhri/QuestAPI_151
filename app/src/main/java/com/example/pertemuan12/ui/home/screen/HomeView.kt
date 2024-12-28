ae4d644f67b2e0b083f8619cbbd4b18a11ef8ec0
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 17:11:40 +0700

    membuat fungsi OnLoading

--- app/src/main/java/com/example/pertemuan12/ui/home/screen/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/home/screen/HomeView.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -30,6 +31,18 @@ import androidx.compose.ui.unit.dp
 import com.example.pertemuan12.R
 import com.example.pertemuan12.model.Mahasiswa
 
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+/**
+ * The home screen displaying error message with re-attempt button.
+ */
 @Composable
 fun OnError(retryAction:()->Unit, modifier: Modifier = Modifier){
     Column(
