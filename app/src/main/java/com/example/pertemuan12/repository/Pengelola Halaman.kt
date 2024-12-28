d0aa227b7e26d24a6e7aac2093fbb77e9a0de350
hilal555 <muhamadhilal04@gmail.com>
2024-12-28 17:39:54 +0700

    membuat fungsi PengelolaHalaman

---
+++ app/src/main/java/com/example/pertemuan12/repository/Pengelola Halaman.kt
@@ -0,0 +1,40 @@
+package com.example.pertemuan12.repository
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.ui.home.screen.DestinasiEntry
+import com.example.pertemuan12.ui.home.screen.DestinasiHome
+import com.example.pertemuan12.ui.home.screen.EntryMhsScreen
+import com.example.pertemuan12.ui.home.screen.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
