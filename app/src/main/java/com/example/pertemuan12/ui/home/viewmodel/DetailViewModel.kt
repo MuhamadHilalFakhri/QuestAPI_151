f4607946b9ff05ff8f859d5cbfd16baa6c6929b3
hilal555 <muhamadhilal04@gmail.com>
2024-12-29 14:56:59 +0700

    membuat fungsi getDetailMahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/DetailViewModel.kt
@@ -2,10 +2,12 @@ package com.example.pertemuan12.ui.home.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan12.model.Mahasiswa
 import com.example.pertemuan12.repository.MahasiswaRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 
 sealed class DetailUiState{
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
@@ -23,7 +25,35 @@ class DetailMhsViewModel(
     private val _detalUiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
     val detailUiState: StateFlow<DetailUiState> = _detalUiState
 
+    init {
+        getDetailMahasiswa()
+    }
 
+    fun getDetailMahasiswa(){
+        viewModelScope.launch {
+            try {
 
+                _detalUiState.value = DetailUiState.Loading
+                val mahasiswa = mhs.getMahasiswabyNim(_nim)
+                if (mahasiswa != null){
+                    _detalUiState.value = DetailUiState.Success(mahasiswa)
+                }else{
+                    _detalUiState.value = DetailUiState.Error
+                }
+            }catch (e:Exception){
+                _detalUiState.value = DetailUiState.Error
+            }
+        }
+    }
+}
 
+fun Mahasiswa.toDetailUiEvent():InsertUiEvent{
+    return InsertUiEvent(
+        nim = nim,
+        nama = nama,
+        alamat = alamat,
+        jenisKelamin = jenisKelamin,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
