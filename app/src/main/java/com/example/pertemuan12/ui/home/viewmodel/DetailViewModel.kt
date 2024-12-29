0049ac4ef8bab4a02326f8cb7587cd7a296532d9
hilal555 <muhamadhilal04@gmail.com>
2024-12-29 14:52:54 +0700

    membuat class DetailMhsViewModel

--- app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/home/viewmodel/DetailViewModel.kt
@@ -1,6 +1,11 @@
 package com.example.pertemuan12.ui.home.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.pertemuan12.model.Mahasiswa
+import com.example.pertemuan12.repository.MahasiswaRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 sealed class DetailUiState{
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
@@ -8,3 +13,17 @@ sealed class DetailUiState{
     object Loading : DetailUiState()
 }
 
+class DetailMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val mhs: MahasiswaRepository
+): ViewModel(){
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
+
+    private val _detalUiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val detailUiState: StateFlow<DetailUiState> = _detalUiState
+
+
+
+
+}
